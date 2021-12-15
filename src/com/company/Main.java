e7f293da115d5ee5437ba0501266ac5264f963e1
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-12-15 13:33:54 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000) {
+            System.out.println("You Score was 5000");
+            System.out.println("This was executed");
+        }
+    }
+}
