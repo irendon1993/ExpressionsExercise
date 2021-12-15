2512625dd42d2eea4f25f0d119c2546f91e8a584
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-12-15 13:38:46 -0500

    Practicing else if statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 5000;
+        int score =800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if (score == 5000) {
-            System.out.println("You Score was 5000");
+        if (score < 5000 && score > 1000) {
+            System.out.println("You Score was under 5000");
             System.out.println("This was executed");
+        } else if (score < 1000) {
+            System.out.println("your Socore was less than 1000");
+        } else {
+            System.out.println("Got here");
         }
     }
 }
